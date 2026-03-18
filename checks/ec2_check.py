4644410b6c4be883da6fa7ba117a47b53a1aee8b
auspicious7 <auspicious.emw@gmail.com>
2026-03-18 17:38:58 +0530

    Update ec2_check.py

--- checks/ec2_check.py
+++ checks/ec2_check.py
@@ -55,28 +55,3 @@ def run(session):
 
 
 
-# def run(session):
-#     inventory = ["\n[ EC2 DETAILS ]\n"]
-#     alerts = ["\n[ EC2 ALERTS ]\n"]
-
-#     ec2 = session.client('ec2')
-#     instances = ec2.describe_instances()
-
-#     for res in instances['Reservations']:
-#         for inst in res['Instances']:
-#             instance_id = inst['InstanceId']
-#             state = inst['State']['Name']
-#             public_ip = inst.get('PublicIpAddress', 'N/A')
-
-#             inventory.append(f"Instance: {instance_id} | State: {state} | Public IP: {public_ip}\n")
-
-#     # Security group check
-#     groups = ec2.describe_security_groups()['SecurityGroups']
-
-#     for group in groups:
-#         for perm in group['IpPermissions']:
-#             for ip in perm.get('IpRanges', []):
-#                 if ip.get('CidrIp') == '0.0.0.0/0':
-#                     alerts.append(f"[!] SG {group['GroupId']} open to world\n")
-
-#     return inventory, alerts
